61b15dc19f7513ed3310b35e2ec0fe4ec1397c9e
khasama <khavl741953@gmail.com>
2024-12-10 18:16:28 +0700

    implement downloader

--- src/index.js
+++ src/index.js
@@ -8,31 +8,42 @@ export async function ixiguaDownloader(url, dir, callback) {
   }
   const match = url.match(/\/(\d+)(?:\?|$)/);
   const id = match ? match[1] : Date.now();
-  const mediaSource = await getIxiguaMediaSource(url);
-  if (mediaSource.audio) {
-    await downloadMedia(mediaSource.audio, path.join(
+  try {
+    const mediaSource = await getIxiguaMediaSource(url);
+    if (!mediaSource.video) {
+      throw new Error('Media source not found!');
+    }
+    if (mediaSource.audio) {
+      await downloadMedia(mediaSource.audio, path.join(
+        dir,
+        `${id}.mp3`
+      ), 'https://www.ixigua.com/', callback);
+    }
+    await downloadMedia(mediaSource.video, path.join(
       dir,
-      `${id}.mp3`
+      `${id}.mp4`
     ), 'https://www.ixigua.com/', callback);
+  } catch (error) {
+    throw error;
   }
-  await downloadMedia(mediaSource.video, path.join(
-    dir,
-    `${id}.mp4`
-  ), 'https://www.ixigua.com/', callback);
 }
 
 export async function douyinDownloader(url, dir, callback) {
   if (!fs.existsSync(dir)) {
     fs.mkdirSync(dir, { recursive: true });
   }
-  const match = url.match(/https:\/\/www\.douyin\.com\/video\/(\d+)/);
-  const id = match ? match[1] : Date.now();
-  const mediaSource = await getDouyinMediaSource(url);
-  if (!mediaSource) {
-    throw new Error('Media source not found!');
+  try {
+    const match = url.match(/https:\/\/www\.douyin\.com\/video\/(\d+)/);
+    const id = match ? match[1] : Date.now();
+    const mediaSource = await getDouyinMediaSource(url);
+    if (!mediaSource) {
+      throw new Error('Media source not found!');
+    }
+    await downloadMedia(mediaSource, path.join(
+      dir,
+      `${id}.mp4`
+    ), 'https://www.douyin.com/', callback);
+  } catch (error) {
+    throw error;
   }
-  await downloadMedia(mediaSource, path.join(
-    dir,
-    `${id}.mp4`
-  ), 'https://www.douyin.com/', callback);
 }
